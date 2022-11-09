e421e88120ea4f1892854cc24bb14fdff7a4808b
benTKam <56923669+benTKam@users.noreply.github.com>
2022-11-09 16:34:08 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,12 +1,14 @@
 import java.util.List;
 
 public class Student {
-    List<Prompt> Prompts;
+    Prompt[] Prompts = new Prompt[3];
     String First;
     String Last;
-    public Student(List<Prompt> prompts, String first, String last){
-        Prompts = prompts;
+    public Student(String first, String last){
         First = first;
         Last = last;
+    }
+    public Prompt[] getStuPrompts(){
+        return Prompts;
     }
 }
